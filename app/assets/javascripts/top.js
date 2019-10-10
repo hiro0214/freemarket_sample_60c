03e1381f54b0d1a180fdcc59a33e2ea898ed5039
hiro0214 <leonhurt2@gmail.com>
2019-10-10 21:50:43 +0900

    page-jump-commit

---
+++ app/assets/javascripts/top.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('a[href^=#]').click(function(){
+    var adjust = 0;
+    var speed = 800;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
+
+
+
+
