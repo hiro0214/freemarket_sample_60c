8a71b45e9de96842d69c77953690db6ba597641c
hiro0214 <leonhurt2@gmail.com>
2019-10-12 18:48:37 +0900

    signup-commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :items 
   get "detail" => "items#detail"
   get "regi_first" => "items#regi_first"
+  get "regi_second" => "items#regi_second"
+  get "regi_third" => "items#regi_third"
   get "exhibit" => "items#exhibit"
   get "mypage" => "items#mypage"
 end
