f5ce5e438da0510d854becbecd87dc72d797419a
hiro0214 <leonhurt2@gmail.com>
2019-10-13 18:11:37 +0900

    signup-fourth-fifth

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "regi_first" => "items#regi_first"
   get "regi_second" => "items#regi_second"
   get "regi_third" => "items#regi_third"
+  get "regi_fourth" => "items#regi_fourth"
+  get "regi_fifth" => "items#regi_fifth"
   get "exhibit" => "items#exhibit"
   get "mypage" => "items#mypage"
 end
