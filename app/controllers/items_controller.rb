0beb62548eea365c4666eeacabb0081ba9cfd3f5
hiro0214 <leonhurt2@gmail.com>
2019-10-15 19:28:51 +0900

    exhibit-commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def exhibit
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_name: item_params[:item_name], 
+                description: item_params[:description],
+                price: item_params[:price],
+                state: item_params[:state],
+                fee_size: item_params[:fee_size],
+                region: item_params[:region],
+                delivery_date: item_params[:delivery_date])
+    if @item.save
+      redirect_to exhibit_after_path
+    end
+  end
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :price, :state, :fee_size, :region, :delivery_date)
+  end
+
+
 end
 
 
