24ff2b05b20cc402f21558c8dd66d9a86ab71258
hiro0214 <leonhurt2@gmail.com>
2019-10-25 20:27:06 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -20,22 +20,12 @@ ActiveRecord::Schema.define(version: 2019_10_23_094843) do
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
-
-  create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_credit_cards_on_user_id"
-
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "item_id", null: false
     t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_images_on_item_id"
-
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -80,16 +70,12 @@ ActiveRecord::Schema.define(version: 2019_10_23_094843) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "credit_cards", "users"
-  add_foreign_key "sns_credentials", "users"
   add_foreign_key "images", "items"
+  add_foreign_key "sns_credentials", "users"
   add_foreign_key "tradings", "items"
 end
