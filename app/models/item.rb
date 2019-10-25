0fff671a40350784e5d9c8e143189d4c88aae971
hiroto302 <i.am.a.hiroto@gmail.com>
2019-10-25 21:01:50 +0900

    fit.2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,18 @@
 class Item < ApplicationRecord
-  has_one :trading
+
+  has_one :trading, dependent: :destroy
   has_one :categories
+  has_many :images, dependent: :destroy
+
 
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length:{maximum: 1000 , message: "must be given please" }
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to:300 }
-  validates :price, numericality: { only_integer: true, less_than:9999999 }
-  validates :state, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to:300, less_than:9999999 }
+  validates :category_index,presence:true
   validates :state, presence: true
   validates :fee_size, presence: true
   validates :region, presence: true
   validates :delivery_date, presence: true
-
-
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
