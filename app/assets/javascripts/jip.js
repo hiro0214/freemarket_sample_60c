95feb8417640819237f0a117fc385d92d04e4eda
hiro0214 <leonhurt2@gmail.com>
2019-10-29 11:00:27 +0900

    jip-commit

--- app/assets/javascripts/jip.js
+++ app/assets/javascripts/jip.js
@@ -2,29 +2,27 @@ $(document).on('turbolinks:load', function() {
 
   $(".jip").keyup(function(){
     var input = $(this).val()
-    if (input.length == 7){
+    var num = input.replace(/[^0-9]/g, '');
+    if (num.length == 7){
       var url = "/signup/jip"
       $.ajax({
         type: "get",
         url: url,
-        data: {input: input},
+        data: {input: num},
         dataType: 'json'
       })
 
       .done(function(data){
-        console.log(data)
-
         $(".prefecture").val(data.prefecture)
         $(".city").val(data.city)
         $(".town").val(data.town)
       })
 
       .fail(function(){
-        console.log("mis")
         $(".prefecture").val("")
         $(".city").val("")
         $(".town").val("")
-        // alert("正しい値を入力して下さい")
+        alert("正しい値を入力して下さい")
       })
     } else {
       $(".prefecture").val("")
