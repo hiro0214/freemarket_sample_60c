9e145f23a628707b9e78093a7f592d14fb0c01bf
hiro0214 <leonhurt2@gmail.com>
2019-10-31 20:50:04 +0900

    basic-commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user! # 追記 (userの部分はdeviseのmodel名)
+
+  #Basic認証
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+
+    private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,3 +61,6 @@
 #   }
 
 server '13.115.148.93', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
