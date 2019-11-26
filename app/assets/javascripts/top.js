eb5f9551d0f7168270b9c790d16901446346e7df
hiro0214 <leonhurt2@gmail.com>
2019-11-26 17:20:08 +0900

    fixed

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,51 +1,58 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', function () {
 
-  $('a[href^="#"]').click(function(){
-    var href= $(this).attr("href");
+  $('a[href^="#"]').click(function () {
+    var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top - 30;
-    $('body,html').animate({scrollTop:position}, 500, 'swing');
+    $('body,html').animate({ scrollTop: position }, 500, 'swing');
     return false;
   });
 
-  $(".header_alert_text > p").animate({left: "600px"}, 4000)
+  $(".header_alert_text > p").animate({ left: "600px" }, 4000)
   $(".header_alert").fadeOut(4000)
 
 
-  function image1(){
-    $(".main__img1").animate({opacity: 0},800)
-    $(".main__img2").animate({opacity: 1},800)
+  function image1() {
+    $(".main__img1").animate({ opacity: 0 }, 800)
+    $(".main__img2").animate({ opacity: 1 }, 800)
     setTimeout(image2, 6400)
   }
 
-  function image2(){
-    $(".main__img1").animate({opacity: 1},800)
-    $(".main__img2").animate({opacity: 0},800)
+  function image2() {
+    $(".main__img1").animate({ opacity: 1 }, 800)
+    $(".main__img2").animate({ opacity: 0 }, 800)
     setTimeout(image1, 6400)
   }
 
   setTimeout(image1, 6000)
 
-  function move(){
+  $(".logo").click(function (e) {
+    e.preventDefault()
+    window.location.href = "/"
+  })
+
+
+
+  function move() {
     window.location.href = "/items/new"
   }
 
-  $(".exhibit__btn").click(function(){
-    $(".exhibit__btn__circle").animate({borderWidth: "10px"}, 200, function(){
-      $(".exhibit__btn__circle").animate({borderWidth: "0px"}, 200)
+  $(".exhibit__btn").click(function () {
+    $(".exhibit__btn__circle").animate({ borderWidth: "10px" }, 200, function () {
+      $(".exhibit__btn__circle").animate({ borderWidth: "0px" }, 200)
       setTimeout(move, 500)
     })
   })
 
-  $(".fortunetelling").click(function(){
-    var val = Math.round( Math.random()*5 );
-    if (val == 5){
+  $(".fortunetelling").click(function () {
+    var val = Math.round(Math.random() * 5);
+    if (val == 5) {
       alert("大吉")
-    } else if(val == 4) {
+    } else if (val == 4) {
       alert("中吉")
-    } else if(val == 3) {
+    } else if (val == 3) {
       alert("小吉")
-    } else if(val == 2) {
+    } else if (val == 2) {
       alert("吉")
     } else {
       alert("大凶")
